42d4f048d9b7bbfcec0c06ded5c34b5b59a230a3
Arinobu FUKUHARA <afukuhara@gmail.com>
2013-02-18 20:44:32 +0900

    added elpa setting files

---
+++ .emacs.d/inits/00_elpa.el
@@ -0,0 +1,7 @@
+;; -------------------------------------------------------
+;;  ELPA setting
+;; -------------------------------------------------------
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
