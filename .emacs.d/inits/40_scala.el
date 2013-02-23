ed5e2083e88e59237bc5d501d7c9c5ba30a5e2b9
Arinobu FUKUHARA <afukuhara@gmail.com>
2013-02-23 12:14:54 +0900

    modified for scala-mode2

--- .emacs.d/inits/40_scala.el
+++ .emacs.d/inits/40_scala.el
@@ -2,45 +2,18 @@
 ;;;                   Scala Programming Settings                     ;;;
 ;;; ================================================================ ;;;
 
-;; ================================================================ ;;;
-;;  From: http://blog.iss.ms/2012/06/02/101357  ;;;
-;; ================================================================ ;;;
-(defun my-scala-newline(arg)
-  (interactive "p")
-  (cond ((scala-in-multi-line-comment-p)
-         (scala-newline))
-        ((char-equal ?\} (following-char))
-         (let (killed)
-           (newline-and-indent)
-           (newline-and-indent)
-           (forward-char)
-           (setq killed (not (my-end-of-line-p)))
-           (if killed (kill-line))
-           (previous-line)
-           (indent-for-tab-command)
-           (if killed (yank))))
-        (t
-         (newline-and-indent))))
+;; ------------------------------------------------------------ ;;;
+;;  From: https://github.com/hvesalai/scala-mode2
+;; ------------------------------------------------------------ ;;;
+;; (require 'scala-mode2)
 
-(require 'scala-mode-feature-electric)
-
-(setq scala-mode-feature:electric-expand-delimiters-list '(?\{))
-
-
-;; ================================================================ ;;;
-;;  From: http://blog.iss.ms/2012/06/02/101357  ;;;
-;; ================================================================ ;;;
-(require 'scala-mode-auto)
+(autoload 'scala-mode "scala-mode2" nil t)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
-(add-hook 'scala-mode-hook
-  (function
-    (lambda ()
-      (setq scala-mode-indent:step 4)
-      (scala-mode-lib:define-keys scala-mode-map
-                                  ([(shift tab)]   'scala-undent-line)
-                                  ([(control tab)] nil))
-      (local-set-key [(return)] 'newline-and-indent)
-      (scala-mode-feature-electric-mode)
-      (define-key scala-mode-map "\r" 'my-scala-newline))))
-(add-hook 'scala-mode-hook 'jaspace-mode)
 
+
+(add-hook
+ 'scala-mode-hook
+ (lambda ()
+   (local-set-key "\C-cc" 'quickrun-with-arg)
+
+   (setq quickrun-timeout-seconds 30)))
