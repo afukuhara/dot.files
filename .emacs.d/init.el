bf9bb0d7367b5e19d1cd1fc26c409a9748324eea
Arinobu FUKUHARA <afukuhara@gmail.com>
2013-02-23 11:26:35 +0900

    add el-get setting.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,10 @@
 ;;;; -*- mode: emacs-lisp; coding: utf-8 -*-
 
-(let ((default-directory "~/.emacs.d/elisp/"))
-  (setq load-path (cons default-directory load-path))
+(let ((default-directory "~/.emacs.d/elisp/")
+      (el-get-directory  "~/.emacs.d/el-get/el-get"))
+  (add-to-list 'load-path
+               '(default-directory el-get-directory))
   (normal-top-level-add-subdirs-to-load-path))
 
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits") ; 設定ファイルがあるディレクトリを指定
-
--- .emacs.d/inits/00_elpa.el
+++ .emacs.d/inits/00_elpa.el
@@ -1,7 +1,19 @@
 ;; -------------------------------------------------------
 ;;  ELPA setting
 ;; -------------------------------------------------------
-
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+
+;; -------------------------------------------------------
+;;  el-get setting
+;; -------------------------------------------------------
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
